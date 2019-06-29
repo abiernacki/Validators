ee532eda74789f267b80f95eda9a760a3a766f52
Adrian Biernacki <adrianbie86@gmail.com>
2019-06-29 20:40:33 +0200

    describe Pesel

--- src/main/java/Pesel.java
+++ src/main/java/Pesel.java
@@ -1,4 +1,9 @@
-
+/*
+ The PESEL number is an identification number given to every Polish citizen
+ when they are born and registered in Poland.
+ Moreover, expats that meet certain criteria or apply for it also can get the PESEL number.
+ Every PESEL number consists of eleven digits.
+ */
 
 public class Pesel {
 
@@ -7,18 +12,18 @@ public class Pesel {
 
         int length = pesel.length();
 
-        // pesel jest mniejszy niż 11 znaków czyli zbyt krótki
+        // the pesel is shorter than eleven characters
         if (length < 11) {
             return false;
         }
 
 
-        // pesel jest większy niż 11 znaków czyli jest zbyt długi
+        // the pesel is longer than eleven characters
         if (length > 11) {
             return false;
         }
 
-        // dzielimy ciąg znaków na znaki
+        // divide the string into single characters
         String[] chars = pesel.split("");
 
         int[] numbers = new int[11];
@@ -26,17 +31,17 @@ public class Pesel {
             numbers[i] = Integer.valueOf(chars[i]);
         }
 
-        // przemnażamy kolejne cyfry przez odpowiednie wagi i dodajemy do siebie
+        // multiply subsequent digits by appropriate weights and add to each other
         int sum = numbers[0] * 1 + numbers[1] * 3 + numbers[2] * 7 + numbers[3] * 9 + numbers[4] * 1 +
                 numbers[5] * 3 + numbers[6] * 7 + numbers[7] * 9 + numbers[8] * 1 + numbers[9] * 3;
 
-        // wynik dzielimy modulo przez 10
+        // divide the result into modulo by ten
         sum = sum%10;
 
-        // od dziesięciu odejmujemy wynik
+        // subtract the result from ten
         sum = 10 - sum;
 
-        // wynik dzielimy modulo 10
+        // divide the result into modulo by ten
         sum = sum%10;
 
         return sum == numbers[10];
